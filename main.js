0ba74e740479c360df567de10f76af7f28c74040
MajorPayne95 <dbhunter4@gmail.com>
2023-04-26 06:36:26 -0500

    Throwing together function ideas

---
+++ main.js
@@ -0,0 +1,17 @@
+let firstNum = document.querySelector('#number')
+
+function add(firstNum, secondNum) {
+    return firstNum + secondNum
+}
+
+function subtract(firstNum, secondNum) {
+    return firstNum - secondNum
+}
+
+function multiply(firstNum, secondNum) {
+    return firstNum * secondNum
+}
+
+function divide(firstNum, secondNum) {
+    return firstNum / secondNum
+}
