233c65d706b956f609748e6e67c8d0416f69ed7e
MajorPayne95 <dbhunter4@gmail.com>
2023-04-26 21:45:03 -0500

    Working on the basic calculator functionality

--- main.js
+++ main.js
@@ -1,6 +1,39 @@
-let firstNum = document.querySelector('#number')
+const number = [...document.getElementsByClassName('number')];
+const operator = [...document.getElementsByClassName('operator')];
+const input = document.getElementById('input');
+const equation = document.getElementById('equation');
+const equals = document.getElementById('equals');
+
+let tempNum;
+let firstNum;
+let secondNum;
+let chosenOperator;
+
+number.forEach(button => 
+    button.addEventListener('click', function() {
+        input.textContent += button.textContent;
+        tempNum = input.textContent;
+    }));
+
+operator.forEach(operator => 
+    operator.addEventListener('click', function() {
+        chosenOperator = operator.textContent;
+        equation.textContent += (tempNum + chosenOperator);
+        firstNum = tempNum;
+        input.textContent = '';
+        tempNum = '';
+}));
+
+equals.addEventListener('click', function() {
+    equation.textContent += tempNum;
+    secondNum = tempNum;
+    if (chosenOperator == '+') {
+        input.textContent = add();
+    };
+})
 
 function add(firstNum, secondNum) {
+    console.log(firstNum + secondNum);
     return firstNum + secondNum
 }
 
